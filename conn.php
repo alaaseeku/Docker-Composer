db393e1bba829edb5cb8c1428282b909b51ffd51
Alaa Ahmed <alaaseeku@gmail.com>
2021-01-29 11:19:55 +0200

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("Appdb", "root", "P@ssw0rd!2020", "mysql");
-
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-?>
